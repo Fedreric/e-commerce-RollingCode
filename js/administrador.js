ffa0605da4cfc377cf3c759177dad68f6e61aaa7
Martinserrano724 <martinserrano724@gmail.com>
2023-05-03 00:25:56 -0300

    Correccion de id de registro

--- js/administrador.js
+++ js/administrador.js
@@ -1,10 +1,11 @@
 //como utilizar codigo de otro archivo JS en un archivo nuevo
-import Pelicula from "./class-Producto.js";
+
+import Producto from "./class-Producto.js";
 //se agrega la llaves ya que no lleva el export default
 
 
 const formulario_Administrador = document.getElementById('formAdministrarPelicula');
-const input_Codigo = document.getElementById('inputCodigo');
+//const input_Codigo = document.getElementById('inputCodigo');
 const input_Nombre = document.getElementById('input-Nombre-Articulo');
 const input_Descripcion = document.getElementById('input-Descripcion-Articulo');
 const input_Imagen = document.getElementById('input-Imagen-Articulo');
@@ -14,7 +15,8 @@ const btn_enviar=document.getElementById('btn-Enviar-Formulario');
 const btn_Agregar=document.getElementById('btn-Agregar');
 let lista_Productos = JSON.parse(localStorage.getItem('lista_Producto'))|| [];
 
-
+let pelicula=new Producto(undefined,'martin','sdasdasadsda','dassdasad','das',123);
+console.log(pelicula);
 
 
 btn_Agregar.addEventListener('click',mostrar_Modal_Producto);
