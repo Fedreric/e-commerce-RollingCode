9bfb0c7117517e6bc9cf57004f6dc36fc8523520
MauroDS <mauro.diaz.santilli@gmail.com>
2023-05-04 16:19:33 -0300

    Borrando modales anteriores y añadiendo margenes a las cards de resultados

--- js/filtroBusqueda.js
+++ js/filtroBusqueda.js
@@ -3,32 +3,40 @@ const inputBuscador = document.getElementById("buscador");
 const resultadoBusqueda = document.getElementById("resultado");
 
 function buscar() {
-    const searchTerm = inputBuscador.value.toLowerCase();
-    const arrayFiltrado = listaProductos.filter(producto =>
-        producto.nombre.toLowerCase().includes(searchTerm)
-    );
-    mostrarProductosBuscados(arrayFiltrado);
+  const terminoBusc = inputBuscador.value.toLowerCase();
+  
+  if (terminoBusc === '') {
+      resultadoBusqueda.innerHTML = '';
+      return;
+  }
+  
+  const arrayFiltrado = listaProductos.filter(producto =>
+      producto.nombre.toLowerCase().includes(terminoBusc)
+  );
+  
+  mostrarProductosBuscados(arrayFiltrado);
 }
 
 function mostrarProductosBuscados(arrayFiltrado) {
-    resultadoBusqueda.innerHTML = '';
+  resultadoBusqueda.innerHTML = '';
 
-    if (arrayFiltrado.length > 0) {
-        arrayFiltrado.forEach(producto => {
-            resultadoBusqueda.innerHTML += `
-            <div class="col-md-6 col-lg-4 card text-bg-light my-3 p-0">
-                <a href="pages/detalle.html">
-                    <img src="${producto.imagen}" alt="${producto.nombre}" class="w-auto img-fluid mt-5">
-                    <div class="card-img-overlay tituloCard ">
-                        <h5 class="card-title">${producto.nombre.toUpperCase()}</h5>
-                    </div>
-                </a>
-            </div>
-            `;
-        });
-    } else {
-        resultadoBusqueda.innerHTML = `<p class="text-center fs-2 fw-light">No se encontraron productos asociados con: ${inputBuscador.value}</p>`;
-    }
+  if (arrayFiltrado.length > 0) {
+      resultadoBusqueda.classList.add('row');
+      arrayFiltrado.forEach(producto => {
+          resultadoBusqueda.innerHTML += `
+          <div class="col-md-4 card text-bg-light my-3 p-0 border border-2 mx-2">
+              <a href="pages/detalle.html">
+              <div class="card-img-overlay">
+                      <h5 class="card-title">${producto.nombre.toUpperCase()}</h5>
+                  </div>
+                  <img src="${producto.imagen}" alt="${producto.nombre}" class="img-fluid mt-5">
+              </a>
+          </div>
+          `;
+      });
+  } else {
+      resultadoBusqueda.innerHTML = `<p class="text-center fs-2 fw-light">No se encontraron productos asociados con: ${inputBuscador.value}</p>`;
+  }
 }
 
 inputBuscador.addEventListener("input", buscar);
