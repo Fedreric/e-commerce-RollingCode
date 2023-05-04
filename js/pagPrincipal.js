14dbe0a38ce15207c4f3db50d594f30f4c277249
MauroDS <mauro.diaz.santilli@gmail.com>
2023-05-04 18:53:20 -0300

    Finalizando migracion y correcciones con filtro de busqueda

--- js/pagPrincipal.js
+++ js/pagPrincipal.js
@@ -108,16 +108,16 @@ function mostrarProductosBuscados(arrayFiltrado) {
       resultadoBusqueda.classList.add('row');
       arrayFiltrado.forEach(producto => {
           resultadoBusqueda.innerHTML += `
-          <div class="col-md-4 card text-bg-light my-3 p-0 border border-2 mx-2">
-            <p class= "text-center fw-light">${producto.categoria.toUpperCase()}</p>
-            <button onclick="window.detalleProducto('${producto.codigo}')">
-              <div class="card-img-overlay">
+          <div class="col-md-4 card text-bg-light my-3 p-0 border border-2 mx-2 d-flex flex-column">
+             <p class="text-center fw-light">${producto.categoria.toUpperCase()}</p>
+             <button class="btn btn-link" onclick="window.detalleProducto('${producto.codigo}')">
+               <div class="card-img-overlay">
                 <h5 class="card-title mt-3 text-center">${producto.nombre.toUpperCase()}</h5>
+               </div>
+             </button>
+                <img src="${producto.imagen}" alt="${producto.nombre}" class="img-fluid mt-5">
               </div>
-            </button>
-            <img src="${producto.imagen}" alt="${producto.nombre}" class="img-fluid mt-5">
-          </div>
-          `;
+           `;
       });
   } else {
       resultadoBusqueda.innerHTML = `<p class="mt-3 text-center fs-3 fw-bold">No se encontraron productos asociados con: ${inputBuscador.value}</p>`;
