18537d3044ab590ca5cfea2549bda00d04552322
MauroDS <mauro.diaz.santilli@gmail.com>
2023-05-03 21:26:37 -0300

    Eliminando comentarios

--- js/filtroBusqueda.js
+++ js/filtroBusqueda.js
@@ -1,23 +1,19 @@
-// obtener los datos del almacenamiento local al cargar la página
 let listaProductos = JSON.parse(localStorage.getItem("listaProductos")) || [];
-
-// obtener referencias a los elementos relevantes del DOM
-const formularioBusqueda = document.getElementById("buscarProducto");
+const filtrarBusqueda = document.getElementById("buscarProducto");
 const inputBuscador = document.getElementById("buscador");
-const productoEncontradoDiv = document.getElementById("producto-encontrado");
+const resultadoBusqueda = document.getElementById("resultado");
 
-// agregar un manejador de eventos para el formulario de búsqueda
-formularioBusqueda.addEventListener("submit", function(event) {
-  // prevenir la recarga de la página
+filtrarBusqueda.addEventListener("submit", function(event) {
+  
   event.preventDefault();
 
-  // buscar el producto en la lista de productos
+ 
   let productoBuscado = listaProductos.find(producto => producto.nombre.toLowerCase() === inputBuscador.value.toLowerCase());
 
   if (productoBuscado) {
-    // crear un elemento para mostrar la información del producto encontrado
-    const productoEncontradoEl = document.createElement("div");
-    productoEncontradoEl.innerHTML = `
+  
+    const productoEncontrado = document.createElement("div");
+    productoEncontrado.innerHTML = `
       <h2>Producto encontrado:</h2>
       <p>Nombre: ${productoBuscado.nombre}</p>
       <p>Descripción: ${productoBuscado.detalle}</p>
@@ -25,11 +21,11 @@ formularioBusqueda.addEventListener("submit", function(event) {
       <p>Stock: ${productoBuscado.stock}</p>
     `;
 
-    // agregar el elemento al DOM
-    productoEncontradoDiv.innerHTML = "";
-    productoEncontradoDiv.appendChild(productoEncontradoEl);
+   
+    resultadoBusqueda.innerHTML = "";
+    resultadoBusqueda.appendChild(productoEncontrado);
   } else {
-    // si no se encuentra el producto, mostrar un mensaje de error
-    productoEncontradoDiv.innerHTML = "<p>No se encontró ningún producto con ese nombre.</p>";
+    
+    resultadoBusqueda.innerHTML = "<p>No se encontró ningún producto con ese nombre.</p>";
   }
 });
