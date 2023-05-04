771012991725f56b6024788d86eb552c2e106970
MauroDS <mauro.diaz.santilli@gmail.com>
2023-05-04 15:34:48 -0300

    Empezando a corregir buscador para hacerlo mas simple

--- js/filtroBusqueda.js
+++ js/filtroBusqueda.js
@@ -1,34 +1,34 @@
 let listaProductos = JSON.parse(localStorage.getItem("listaProductos")) || [];
-const filtrarBusqueda = document.getElementById("buscarProducto");
 const inputBuscador = document.getElementById("buscador");
 const resultadoBusqueda = document.getElementById("resultado");
 
-filtrarBusqueda.addEventListener("submit", function(event) {
-    event.preventDefault();
-    let productoBuscado = listaProductos.find(producto => producto.nombre.toLowerCase() === inputBuscador.value.toLowerCase());
-    if (productoBuscado) {
-      const modalProductoEncontrado = new bootstrap.Modal(document.getElementById('modalpEncontrado'), {});
-      
-      document.getElementById("modalBody").innerHTML = "";
-      
-      const productoEncontrado = document.createElement("div");
-      productoEncontrado.innerHTML = `
-        <p>Nombre: ${productoBuscado.nombre}</p>
-        <p>Descripción: ${productoBuscado.detalle}</p>
-        <p>Categoría: ${productoBuscado.categoria}</p>
-        <p>Stock: ${productoBuscado.stock}</p>
-      `;
-      
-      document.getElementById("modalBody").appendChild(productoEncontrado);
-      modalProductoEncontrado.show();
+function buscar() {
+    const searchTerm = inputBuscador.value.toLowerCase();
+    const arrayFiltrado = listaProductos.filter(producto =>
+        producto.nombre.toLowerCase().includes(searchTerm)
+    );
+    mostrarProductosBuscados(arrayFiltrado);
+}
+
+function mostrarProductosBuscados(arrayFiltrado) {
+    resultadoBusqueda.innerHTML = '';
+
+    if (arrayFiltrado.length > 0) {
+        arrayFiltrado.forEach(producto => {
+            resultadoBusqueda.innerHTML += `
+            <div class="col-md-6 col-lg-4 card text-bg-light my-3 p-0">
+                <a href="pages/detalle.html">
+                    <img src="${producto.imagen}" alt="${producto.nombre}" class="w-auto">
+                    <div class="card-img-overlay tituloCard ">
+                        <h5 class="card-title">${producto.nombre.toUpperCase()}</h5>
+                    </div>
+                </a>
+            </div>
+            `;
+        });
     } else {
-      const modalProductoNoEncontrado = new bootstrap.Modal(document.getElementById('modalpNoEncontrado'), {});
-      modalProductoNoEncontrado.show();
+        resultadoBusqueda.innerHTML = `<p class="text-center fs-2 fw-light">No se encontraron productos asociados con: ${inputBuscador.value}</p>`;
     }
-  });
+}
 
-  const botonDetalle = document.getElementById("btnDetalle");
-
-botonDetalle.addEventListener("click", function() {
-  window.location.href = "./pages/detalle.html";
-});
+inputBuscador.addEventListener("input", buscar);
