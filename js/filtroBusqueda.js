36decc201eba217de9ccbfe3815e716b407f925b
MauroDS <mauro.diaz.santilli@gmail.com>
2023-05-04 00:37:05 -0300

    Corrigiendo error que hacia que los datos cargados se dupliquen cada vez que se aparecia el modal

--- js/filtroBusqueda.js
+++ js/filtroBusqueda.js
@@ -4,26 +4,28 @@ const inputBuscador = document.getElementById("buscador");
 const resultadoBusqueda = document.getElementById("resultado");
 
 filtrarBusqueda.addEventListener("submit", function(event) {
-  event.preventDefault();
-  let productoBuscado = listaProductos.find(producto => producto.nombre.toLowerCase() === inputBuscador.value.toLowerCase());
-  if (productoBuscado) {
-    // Mostrar el modal de producto encontrado
-    const modalProductoEncontrado = new bootstrap.Modal(document.getElementById('productoEncontradoModal'), {});
-    
-    const productoEncontrado = document.createElement("div");
-    productoEncontrado.innerHTML = `
-      <h2>Producto encontrado:</h2>
-      <p>Nombre: ${productoBuscado.nombre}</p>
-      <p>Descripción: ${productoBuscado.detalle}</p>
-      <p>Categoría: ${productoBuscado.categoria}</p>
-      <p>Stock: ${productoBuscado.stock}</p>
-    `;
-    
-    document.getElementById("modalBody").appendChild(productoEncontrado);
-    modalProductoEncontrado.show();
-  } else {
-    // Mostrar el modal de producto no encontrado
-    const modalProductoNoEncontrado = new bootstrap.Modal(document.getElementById('productoNoEncontradoModal'), {});
-    modalProductoNoEncontrado.show();
-  }
-});
+    event.preventDefault();
+    let productoBuscado = listaProductos.find(producto => producto.nombre.toLowerCase() === inputBuscador.value.toLowerCase());
+    if (productoBuscado) {
+      // Mostrar el modal de producto encontrado
+      const modalProductoEncontrado = new bootstrap.Modal(document.getElementById('productoEncontradoModal'), {});
+      
+      // Eliminar el contenido anterior del modal
+      document.getElementById("modalBody").innerHTML = "";
+      
+      const productoEncontrado = document.createElement("div");
+      productoEncontrado.innerHTML = `
+        <p>Nombre: ${productoBuscado.nombre}</p>
+        <p>Descripción: ${productoBuscado.detalle}</p>
+        <p>Categoría: ${productoBuscado.categoria}</p>
+        <p>Stock: ${productoBuscado.stock}</p>
+      `;
+      
+      document.getElementById("modalBody").appendChild(productoEncontrado);
+      modalProductoEncontrado.show();
+    } else {
+      // Mostrar el modal de producto no encontrado
+      const modalProductoNoEncontrado = new bootstrap.Modal(document.getElementById('productoNoEncontradoModal'), {});
+      modalProductoNoEncontrado.show();
+    }
+  });
