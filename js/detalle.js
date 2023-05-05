f131db81885842e859657a7ed80ea1861c041a3a
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-05-05 16:08:46 -0300

    se consigue deshabilitar el boton del agregar al carrito al llegar a 0

--- js/detalle.js
+++ js/detalle.js
@@ -7,27 +7,32 @@ const producto = listaProductos.find(prod => prod.codigo === codigoProd.get('cod
 //mostrar los datos del producto
 const detalle = document.getElementById('contenedorDetalle');
 
-detalle.innerHTML = `        
-<aside class="col-md-6">
-    <img
-        src="${producto.imagen}"
-        alt="${producto.categoria}, ${producto.nombre}"
-        class="w-100"
-    />
-</aside>
-<aside class="col-md-6">
-    <h1>${producto.categoria} - ${producto.nombre}</h1>
-    <hr />
-    <p>
-        ${producto.detalle}
-    </p>
-    <div class="my-3">
-        <span class="precio-poducto">$${producto.precio}</span>
-    </div>
-    <button class="boton text-light my-3" onClick="agregarCarrito('${producto.codigo}')">Agregar al carrito</button>
-    <span class="stock">Stock: ${producto.stock}</span>
-</aside> 
-`
+readDetalle();
+
+function readDetalle(){
+    detalle.innerHTML = `        
+    <aside class="col-md-6">
+        <img
+            src="${producto.imagen}"
+            alt="${producto.categoria}, ${producto.nombre}"
+            class="w-100"
+        />
+    </aside>
+    <aside class="col-md-6">
+        <h1>${producto.categoria} - ${producto.nombre}</h1>
+        <hr />
+        <p>
+            ${producto.detalle}
+        </p>
+        <div class="my-3">
+            <span class="precio-poducto">$${producto.precio}</span>
+        </div>
+        <button class="boton text-light my-3"onClick="agregarCarrito('${producto.codigo}')" id="btnAgregarCarrito">Agregar al carrito</button>
+        <span class="stock">Stock: ${producto.stock}</span>
+    </aside> 
+    `
+}
+
 function agregarCarrito(codigo){
     Swal.fire({
         position: 'top-end',
@@ -44,12 +49,17 @@ function agregarProductoCarritoLocalStorage(){
     localStorage.setItem("listaCarrito", JSON.stringify(listaCarrito));
 }
 function controlStock(codigo){
+    const btnAgregarCarrito = document.getElementById('btnAgregarCarrito');
     //se consigue la posicion del producto en la lista de los productos en el localstorage
     let posicionProducto = listaProductos.findIndex(producto => producto.codigo === codigo)
-    console.log(posicionProducto);
-    //se resta en uno el stock
-    listaProductos[posicionProducto].stock = parseInt(listaProductos[posicionProducto].stock) - 1;
-    //se guarda el nuevo valor en el local storage
-    localStorage.setItem("listaProductos", JSON.stringify(listaProductos));
-    
+    let productoStock = parseInt(listaProductos[posicionProducto].stock);
+    if(parseInt(listaProductos[posicionProducto].stock) === 0){
+        btnAgregarCarrito.className = "boton text-light my-3 btn-deshabilitado"
+    }else{
+        //se resta en uno el stock
+        listaProductos[posicionProducto].stock = productoStock - 1;
+        //se guarda el nuevo valor en el local storage
+        localStorage.setItem("listaProductos", JSON.stringify(listaProductos));  
+        readDetalle(); 
+    }
 }
