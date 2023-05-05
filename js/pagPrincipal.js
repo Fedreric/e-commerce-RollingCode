794835af34052652c1ebd57149cfc56581c25040
MauroDS <mauro.diaz.santilli@gmail.com>
2023-05-05 00:36:47 -0300

    Corrigiendo una tipografia y maquetando para que se muestren 3 cards en una misma fila

--- js/pagPrincipal.js
+++ js/pagPrincipal.js
@@ -105,21 +105,23 @@ function mostrarProductosBuscados(arrayFiltrado) {
   resultadoBusqueda.innerHTML = '';
 
   if (arrayFiltrado.length > 0) {
-      resultadoBusqueda.classList.add('row');
-      arrayFiltrado.forEach(producto => {
-          resultadoBusqueda.innerHTML += `
-          <div class="col-md-4 card text-bg-light my-3 p-0 border border-2 mx-2 d-flex flex-column">
-             <p class="text-center fw-light">${producto.categoria.toUpperCase()}</p>
+    resultadoBusqueda.classList.add('row', 'justify-content-center');
+    arrayFiltrado.forEach(producto => {
+        resultadoBusqueda.innerHTML += `
+        <div class="col-12 col-md-3 col-lg-3 card text-bg-light mx-4 my-3 p-0 border border-2 d-flex flex-column h-100">
+           <div class="text-center">
+             <p class="fw-light">${producto.categoria.toUpperCase()}</p>
              <button class="btn btn-link" onclick="window.detalleProducto('${producto.codigo}')">
                <div class="card-img-overlay">
-                <h5 class="card-title mt-3 text-center">${producto.nombre.toUpperCase()}</h5>
+                <h5 class="card-title mt-3">${producto.nombre.toUpperCase()}</h5>
                </div>
              </button>
-                <img src="${producto.imagen}" alt="${producto.nombre}" class="img-fluid mt-5">
-              </div>
-           `;
-      });
-  } else {
-      resultadoBusqueda.innerHTML = `<p class="mt-3 text-center fs-3 fw-bold">No se encontraron productos asociados con: ${inputBuscador.value}</p>`;
-  }
+           </div>
+              <img src="${producto.imagen}" alt="${producto.nombre}" class="img-fluid mt-5">
+        </div>
+         `;
+    });
+} else {
+    resultadoBusqueda.innerHTML = `<p class="mt-3 text-center fs-3 fw-bold">No se encontraron productos asociados con: ${inputBuscador.value}</p>`;
+}
 }
