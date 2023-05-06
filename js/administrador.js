1ed0965b9ac27070f865972b9bf668064f4f415b
MauroDS <mauro.diaz.santilli@gmail.com>
2023-05-06 19:08:56 -0300

    Finalizando logica de validacion. Ampliar mas info sobre los requerimientos que debe introducir el usuario

--- js/administrador.js
+++ js/administrador.js
@@ -1,4 +1,5 @@
 import Producto from "./class-Producto.js";
+import { conjuntoValidaciones } from "./helpers.js";
 
 const formularioAdministrador = document.getElementById(
   "formAdministrarProducto"
@@ -9,8 +10,9 @@ const inputDescripcion = document.getElementById("inputDescripcionArticulo");
 const inputImagen = document.getElementById("inputImagenArticulo");
 const inputCategoria = document.getElementById("inputCategoriaArticulo");
 const inputStock = document.getElementById("inputStockArticulo");
-const inputPreio = document.getElementById("inputPrecioArticulo");
+const inputPrecio = document.getElementById("inputPrecioArticulo");
 const btnAgregar = document.getElementById("btnAgregar");
+const msjFormulario = document.getElementById('msjFormulario');
 
 let estadoProducto = true; // se crea el producto en 'true', se edita el producto en 'false'
 
@@ -44,6 +46,9 @@ function cargaProductos(e) {
 }
 
 function crearProducto (){
+  let validaciones = conjuntoValidaciones(inputNombre,inputDescripcion,inputImagen,inputCategoria,inputStock,inputPrecio)
+  if(validaciones.length === 0){
+
   let nuevoProducto = new Producto(
     undefined,
     inputNombre.value,
@@ -51,7 +56,7 @@ function crearProducto (){
     inputImagen.value,
     inputCategoria.value,
     inputStock.value,
-    inputPreio.value
+    inputPrecio.value
   );
   listaProductos.push(nuevoProducto);
   guardarProductosLocalStorage();
@@ -65,11 +70,20 @@ function crearProducto (){
     'El producto fue agregado con exito',
     'success'
   )
+}else{
+  msjFormulario.className = 'alert alert-danger mt-3';
+        msjFormulario.innerHTML = validaciones;
+        setTimeout(() => {
+            msjFormulario.style.display = 'none'
+        },3000)
+        msjFormulario.style.display = 'block'
 }
 //funcion para guardar los datos en la key lista producto
 function guardarProductosLocalStorage() {
   localStorage.setItem("listaProductos", JSON.stringify(listaProductos));
 }
+   }
+
 //nos sirve para resetar los datos del formulario
 function limpiarFormulario() {
   formularioAdministrador.reset();
@@ -162,7 +176,7 @@ window.editarProducto = (codigoUnico) => {
   inputImagen.value = producto.imagen;
   inputCategoria.value = producto.categoria;
   inputStock.value = producto.stock;
-  inputPreio.value = producto.precio;
+  inputPrecio.value = producto.precio;
   
   // cambio el estado de la variable bandera
   estadoProducto = false;
@@ -192,7 +206,7 @@ datosTabla.children[posicionProducto].children[0].innerHTML= `<img src="${inputI
 datosTabla.children[posicionProducto].children[1].innerHTML= inputNombre.value;
 datosTabla.children[posicionProducto].children[2].innerHTML= inputCategoria.value;
 datosTabla.children[posicionProducto].children[3].innerHTML= inputDescripcion.value;
-datosTabla.children[posicionProducto].children[4].innerHTML= inputPreio.value;
+datosTabla.children[posicionProducto].children[4].innerHTML= inputPrecio.value;
 datosTabla.children[posicionProducto].children[5].innerHTML= inputStock.value;
 
 //mostrar mensaje
@@ -208,6 +222,6 @@ Swal.fire(
 //cerrar modal
 modalProducto.hide();
 
-
  
 }
+
--- js/helpers.js
+++ js/helpers.js
@@ -1,41 +1,92 @@
-export function conjuntoValidaciones(inputNombre, inputDescripcion, inputImagen, inputCategoria, inputStock, inputPrecio) {
-    let errores = [];
-  
-    if (inputNombre.trim() === '') {
-        errores.push('El nombre es requerido');
-        document.getElementById('msjNombre').textContent = 'El nombre es requerido';
-      } else {
-        document.getElementById('msjNombre').textContent = '';
-      }
-    
-      if (inputDescripcion.trim() === '') {
-        errores.push('La descripción es requerida');
-        document.getElementById('msjDescripcion').textContent = 'La descripción es requerida';
-      } else {
-        document.getElementById('msjDescripcion').textContent = '';
-      }
-    
-      if (inputImagen.trim() === '') {
-        errores.push('La imagen es requerida');
-        document.getElementById('msjImagen').textContent = 'La imagen es requerida';
-      } else {
-        document.getElementById('msjImagen').textContent = '';
-      }
-    
-      if (inputCategoria.trim() === '') {
-        errores.push('La categoría es requerida');
-        document.getElementById('msjCategoria').textContent = 'La categoría es requerida';
-      } else {
-        document.getElementById('msjCategoria').textContent = '';
-      }
-    
-      if (isNaN(inputStock) || inputStock < 0) {
-        erroresMsj.push('El stock debe ser un número positivo');
-      }
-      
-      if (isNaN(inputPrecio) || inputPrecio <= 0) {
-        erroresMsj.push('El precio debe ser un número positivo mayor que cero');
-      }
+export function cantidadCaracteres(texto,min,max){
+    if(texto.length >= min && texto.length <= max)
+    {
+        console.log('Cantidad de caracteres correcto');
+        return true;
+    }else{
+        console.log('Cantidad de caracteres incorrecto');
+        return false;
     }
+}
 
-    return erroresMsj
+function validarURLImagen(value){
+    let patron = /^(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|webp)$/
+    if(patron.test(value)){
+        console.log('Url valida');
+        return true;
+    }else{
+        console.log('Url invalida');
+        return false;
+    }
+}
+function validarStock(value){
+    let patron = /^[0-9]{1,3}$/;
+    if(patron.test(value))
+    {
+        console.log('Digito valido de 1 a 3 caracteres');
+        return true;
+    }
+    else{
+        console.log('Numero de digitos invalido')
+        return false;
+    }
+}
+
+function validarCategoria(texto){
+    if(texto.length > 0 && (texto === 'Teclado' || texto  === 'Monitor' || texto === 'Mouse' || texto === 'Fuente' || texto === 'Combo Teclado + Mouse' || texto === 'Memorias Ram' || texto === 'Parlante' || texto === 'Auricular' || texto === 'Disco Duro')){
+        console.log('Categoria valida')
+        return true;
+    }
+    else{
+        console.log('Categoria invalida')
+        return false;
+    }
+}
+
+function validarPrecio(value){
+    let patron = /^[0-9]{1,6}$/;
+    if(patron.test(value))
+    {
+        console.log('Digito valido de 1 a 6 caracteres');
+        return true;
+    }
+    else{
+        console.log('Numero de digitos invalido')
+        return false;
+    }
+}
+
+
+export function conjuntoValidaciones(inputNombre,inputDescripcion,inputImagen,inputCategoria,inputStock,inputPrecio){
+    let validacion = '';
+    if(!cantidadCaracteres(inputNombre,3,100))
+    {
+        validacion += 'Debes corregir el campo del nombre. Este debe contener entre 3 y 100 caracteres <br>';
+    }
+    if(!cantidadCaracteres(inputDescripcion,10,500))
+    {
+        validacion += 'Debes corregir la cantidad de caracteres de la descripción <br>';
+    }
+    if(inputStock.length !== 0 && !validarStock(inputStock))
+    {
+        validacion += 'Corregir el stock, debe ser un numero de 3 digitos como maximo <br>';
+    }
+    if(inputPrecio.length !== 0 && !validarPrecio(inputPrecio))
+    {
+        validacion += 'Corregir el precio, debe ser un numero de 6 digitos como maximo <br>';
+    }
+    if(!validarURLImagen(inputImagen))
+    {
+        validacion += 'Corregir la URL de la imagen, la extension debe ser .jpg, .gif, .png o .webp <br>';
+    }
+    if(!validarCategoria(inputCategoria)){
+        validacion += 'Seleccione un categoria de la lista de opciones de productos <br>'
+    }
+    
+    if(validacion.length !== 0){
+        return validacion;
+    }else{
+        console.log('El formulario es correcto');
+        return '';
+    }
+}
