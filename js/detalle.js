4130d9d73713bd0f1a71c98fc4c94783cd38c4ba
Martinserrano724 <martinserrano724@gmail.com>
2023-05-07 00:30:51 -0300

    Ordenamiento del codigo de comentario

--- js/detalle.js
+++ js/detalle.js
@@ -1,21 +1,34 @@
 //extraer el codigo del producto del url
 const codigoProd = new URLSearchParams(window.location.search);
 //buscar producto
-let listaProductos = JSON.parse(localStorage.getItem('listaProductos')) || [];
+let listaProductos = JSON.parse(localStorage.getItem("listaProductos")) || [];
 let listaCarrito = JSON.parse(localStorage.getItem("listaCarrito")) || [];
-const producto = listaProductos.find(prod => prod.codigo === codigoProd.get('codigo'));
+const producto = listaProductos.find(
+  (prod) => prod.codigo === codigoProd.get("codigo")
+);
 //mostrar los datos del producto
-const detalle = document.getElementById('contenedorDetalle');
+const detalle = document.getElementById("contenedorDetalle");
 let contadorCarrito = document.getElementById("contadorCarrito");
-const btnAgregarCarrito = document.getElementById('btnAgregarCarrito');
-const agotado = document.getElementById('stock')
+const btnAgregarCarrito = document.getElementById("btnAgregarCarrito");
+const agotado = document.getElementById("stock");
+//variables comentario
+const formularioComentairo = document.getElementById("formularioComentario");
+const inputNombre = document.getElementById("nameComentario");
+const inputComentario = document.getElementById("comentario");
 
+let listaComentario = JSON.parse(localStorage.getItem("listaComentario")) || [];
+let date = new Date().toDateString();
+let horas = new Date();
+let horaActual =
+  horas.getHours() + " : " + horas.getMinutes() + " : " + horas.getSeconds();
+
+formularioComentairo.addEventListener("submit", cargaComentario);
 
 readDetalle();
 contadorCarritoAct();
-function readDetalle(){
-    if(parseInt(producto.stock) === 0){
-        detalle.innerHTML = `        
+function readDetalle() {
+  if (parseInt(producto.stock) === 0) {
+    detalle.innerHTML = `        
     <aside class="col-md-6">
         <img
             src="${producto.imagen}"
@@ -36,8 +49,8 @@ function readDetalle(){
         <span class="stock" id="stock">AGOTADO!</span>
     </aside> 
     `;
-    }else{
-        detalle.innerHTML = `        
+  } else {
+    detalle.innerHTML = `        
     <aside class="col-md-6">
         <img
             src="${producto.imagen}"
@@ -58,106 +71,98 @@ function readDetalle(){
         <span class="stock" id="stock">Stock: ${producto.stock}</span>
     </aside> 
     `;
-    }
+  }
 }
 
-function agregarCarrito(){
-    let productoStock = parseInt(producto.stock);
-    //confirma que haya productos en el stock y recien carga el prod al carrito
-    if(productoStock > 0){
-        Swal.fire({
-            position: 'top-end',
-            icon: 'success',
-            title: 'Listo <i class="bi bi-cart"></i>',
-            showConfirmButton: false,
-            timer: 1500
-          })
-        listaCarrito.push(producto);
-        agregarProductoCarritoLocalStorage();
-        //se resta en uno el stock
-        producto.stock = productoStock - 1;
-        //se guarda el nuevo valor en el local storage
-        localStorage.setItem("listaProductos", JSON.stringify(listaProductos));  
-        readDetalle(); 
-        contadorCarritoAct();
-    }
+function agregarCarrito() {
+  let productoStock = parseInt(producto.stock);
+  //confirma que haya productos en el stock y recien carga el prod al carrito
+  if (productoStock > 0) {
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: 'Listo <i class="bi bi-cart"></i>',
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    listaCarrito.push(producto);
+    agregarProductoCarritoLocalStorage();
+    //se resta en uno el stock
+    producto.stock = productoStock - 1;
+    //se guarda el nuevo valor en el local storage
+    localStorage.setItem("listaProductos", JSON.stringify(listaProductos));
+    readDetalle();
+    contadorCarritoAct();
+  }
 }
-function agregarProductoCarritoLocalStorage(){
-    localStorage.setItem("listaCarrito", JSON.stringify(listaCarrito));
+function agregarProductoCarritoLocalStorage() {
+  localStorage.setItem("listaCarrito", JSON.stringify(listaCarrito));
 }
 
 //modifica el span en el maquetado con la cantidad de productos cargados en el carrito
-function contadorCarritoAct(){
-    contadorCarrito.innerText = listaCarrito.length;
-  }
+function contadorCarritoAct() {
+  contadorCarrito.innerText = listaCarrito.length;
+}
 
-  // logica comentarios
-  const formularioComentairo=document.getElementById('formularioComentario');
-  const inputNombre=document.getElementById('nameComentario');
-  const inputComentario=document.getElementById('comentario');
+// logica comentarios
 
-  let listaComentario=JSON.parse(localStorage.getItem("listaComentario")) || [];
-  let date = new Date().toDateString();
-  let horas= new Date();
-  let horaActual=horas.getHours()+' : '+horas.getMinutes()+' : '+horas.getSeconds();
-  
-  formularioComentairo.addEventListener('submit',cargaComentario)
-  
-    // logica comentarios
-    class Cometarios{
-      constructor(nombre,codigo,comentario ,fecha ,hora){
-          this.nombre=nombre;
-          this.codigo=codigo;
-          this.comentario=comentario;
-          this.fecha=fecha;
-          this.hora=hora;
-      }
+class Cometarios {
+  constructor(nombre, codigo, comentario, fecha, hora) {
+    this.nombre = nombre;
+    this.codigo = codigo;
+    this.comentario = comentario;
+    this.fecha = fecha;
+    this.hora = hora;
   }
-  
-  
-  cargaDeDatosInicial();
-  function cargaComentario(e) {
-      e.preventDefault();
-      crearComentario(); 
-    }
-    function cargarComentariosLocalStorage(){
-          localStorage.setItem("listaComentario", JSON.stringify(listaComentario));
-    }
-    function limpiarFormulario() {
-      formularioComentairo.reset();
-    }
-    
-    function crearComentario() {
-        let nuevoComentario = new Cometarios(inputNombre.value,codigoProd.get('codigo'),inputComentario.value,date,horaActual);
-        listaComentario.push(nuevoComentario);
-        cargarComentariosLocalStorage();
-        limpiarFormulario();
-         
-        dibujarComentarios(nuevoComentario)   
-    }
-    
-  
-  
-    function cargaDeDatosInicial() {
-      console.log(listaComentario);
-      console.log(listaComentario.log);
-      if (listaComentario.length > 0) {
-        //recorre el array y en cada iten del array llama la funcion dibujar para poder dibujar las filas correspondientes
-        {
-        
-          listaComentario.map((coment) =>{if(coment.codigo== codigoProd.get('codigo')) {dibujarComentarios(coment)}})
-        
-        
+}
+
+cargaDeDatosInicial();
+function cargaComentario(e) {
+  e.preventDefault();
+  crearComentario();
+}
+function cargarComentariosLocalStorage() {
+  localStorage.setItem("listaComentario", JSON.stringify(listaComentario));
+}
+function limpiarFormulario() {
+  formularioComentairo.reset();
+}
+
+function crearComentario() {
+  let nuevoComentario = new Cometarios(
+    inputNombre.value,
+    codigoProd.get("codigo"),
+    inputComentario.value,
+    date,
+    horaActual
+  );
+  listaComentario.push(nuevoComentario);
+  cargarComentariosLocalStorage();
+  limpiarFormulario();
+
+  dibujarComentarios(nuevoComentario);
+}
+
+function cargaDeDatosInicial() {
+  console.log(listaComentario);
+  console.log(listaComentario.log);
+  if (listaComentario.length > 0) {
+    //recorre el array y en cada iten del array llama la funcion dibujar para poder dibujar las filas correspondientes
+    {
+      listaComentario.map((coment) => {
+        if (coment.codigo == codigoProd.get("codigo")) {
+          dibujarComentarios(coment);
         }
-        
-      }
+      });
     }
-    //funcion para dibujar las filas de los producto
-    function dibujarComentarios(coment) {
-      let datosComentarios = document.getElementById("contenedorComentarios");
-      //aqui se dubuja la tabla
-      
-      datosComentarios.innerHTML += `<hr><div class="comentario my-1 m-5">
+  }
+}
+//funcion para dibujar las filas de los producto
+function dibujarComentarios(coment) {
+  let datosComentarios = document.getElementById("contenedorComentarios");
+  //aqui se dubuja la tabla
+
+  datosComentarios.innerHTML += `<hr><div class="comentario my-1 m-5">
       <h6 class="m-0 p-0"> ${coment.nombre}</h6>
       <p class="m-0 p-0 fst-italic ">
       ${coment.hora} ${coment.fecha} 
@@ -167,5 +172,4 @@ function contadorCarritoAct(){
       </p>
     </div>
      `;
-    }
-  
+}
