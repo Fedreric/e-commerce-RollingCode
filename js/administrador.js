e70e68083fe1d563b19c175fc1c99272840c2137
MauroDS <mauro.diaz.santilli@gmail.com>
2023-05-06 22:41:15 -0300

    Eliminando el tiempo del mensaje de error

--- js/administrador.js
+++ js/administrador.js
@@ -72,10 +72,8 @@ function crearProducto() {
   } else {
     msjFormulario.className = 'alert alert-danger mt-3';
     msjFormulario.innerHTML = validaciones;
-    setTimeout(() => {
-      msjFormulario.style.display = 'none'
-    }, 15000)
-msjFormulario.style.display = 'block'
+    msjFormulario.style.display = 'block'
+      
   }
 }
 //funcion para guardar los datos en la key lista producto
